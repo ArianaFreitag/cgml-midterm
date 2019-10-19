6d2a200b598314cee0e833f10a94df47e8234822
Ariana Freitag <freitagariana@gmail.com>
2019-10-19 13:36:35 -0400

    init: make resnet50 file and scaffold functions

---
+++ resnet50.py
@@ -0,0 +1,39 @@
+#!/bin/python3.6
+'''
+Implementation of Resnet-50 from https://arxiv.org/pdf/1512.03385.pdf
+Minyoung Na and Ariana Freitag
+'''
+
+import math
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.datasets import cifar100
+
+class Data(object):
+    '''
+    get the cifar100 data ready
+    '''
+
+    def __init__(self):
+
+def identityBlock():
+    '''
+    block used in the shortcut
+
+    '''
+
+def convBlock():
+    '''
+    conv block not used in the shortcut
+    '''
+
+def resNet50():
+    '''
+    putting everything together
+    '''
+
+if __name__ == "__main__":
+    '''
+    train the model here
+    '''
