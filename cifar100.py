5ba915614cbe4df873f9465db0b8bbb6c9bb49b6
Ariana Freitag <freitagariana@gmail.com>
2019-10-27 19:04:32 -0400

    formatted

--- cifar100.py
+++ cifar100.py
@@ -17,32 +17,32 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-'''
+"""
 Implementation of Resnet-50 from "Deep Residual Learning for Image Recognition" (https://arxiv.org/pdf/1512.03385.pdf)
 and "Identity Mappings in Deep Residual Networks" (https://arxiv.org/abs/1603.05027.pdf). More specicifally
 our block will look like this https://raw.githubusercontent.com/raghakot/keras-resnet/master/images/residual_block.png
 
 Minyoung Na and Ariana Freitag
-'''
+"""
 # Training parameters
-batch_size = 128
+batch_size = 100
 epochs = 200
 num_classes = 100
-depth= 50
+depth = 50
 
 # hyperparam
-BLCKSIZE = 3 # h and w of block
-KEEPPROB = 0.9 # probability of activation unit being kept
+BLCKSIZE = 3  # h and w of block
+KEEPPROB = 0.9  # probability of activation unit being kept
 
 # Subtracting pixel mean improves accuracy
 subtract_pixel_mean = True
 
-model_type = 'ResNet%d' % (depth)
+model_type = "ResNet%d" % (depth)
 
 (x_train, y_train), (x_test, y_test) = cifar100.load_data()
 input_shape = x_train.shape[1:]
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
 
 x_train_mean = np.mean(x_train, axis=0)
 x_train -= x_train_mean
@@ -51,6 +51,7 @@ x_test -= x_train_mean
 
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
+
 
 def lr_schedule(epoch):
     lr = 1e-3
@@ -62,11 +63,12 @@ def lr_schedule(epoch):
         lr *= 1e-2
     elif epoch > 80:
         lr *= 1e-1
-    print('Learning rate: ', lr)
+    print("Learning rate: ", lr)
     return lr
 
+
 def drop_block(A):
-    '''
+    """
     Inputs:
     A is the weights from the activation layer with shape [batch, row, col, chan]
     mode turns off drop block during inference
@@ -75,69 +77,84 @@ def drop_block(A):
     activation layer with correlated blocks dropped
 
     Implementation from: https://arxiv.org/pdf/1810.12890.pdf
-    '''
+    """
 
     block_size = BLCKSIZE
     keep_prob = KEEPPROB
     feat_size = A.shape[2]
-    input_shape = [batch_size, feat_size-block_size+1, feat_size-block_size+1, A.shape[3]]
-
+    input_shape = [
+        batch_size,
+        feat_size - block_size + 1,
+        feat_size - block_size + 1,
+        A.shape[3],
+    ]
 
     # compute gamma
-    term_1 = (1 - keep_prob) / (block_size)**2
-    term_2 = (feat_size)**2 / (feat_size-block_size+1)**2
+    term_1 = (1 - keep_prob) / (block_size) ** 2
+    term_2 = (feat_size) ** 2 / (feat_size - block_size + 1) ** 2
     gamma = term_1 * term_2
 
-    #set padding
+    # set padding
     p1 = (block_size - 1) // 2
     p0 = (block_size - 1) - p1
     padding = [[0, 0], [p0, p1], [p0, p1], [0, 0]]
 
     # create mask from Bernoulli distribution
-    block_mask = tf.nn.relu(tf.sign(gamma - tf.random.uniform(input_shape, minval=0, maxval=1, dtype=tf.float32)))
+    block_mask = tf.nn.relu(
+        tf.sign(
+            gamma - tf.random.uniform(input_shape, minval=0, maxval=1, dtype=tf.float32)
+        )
+    )
     block_mask = tf.pad(block_mask, padding)
 
     # place mask on A
-    block_mask = 1 - tf.nn.max_pool(block_mask, [1, block_size, block_size, 1], [1, 1, 1, 1], 'SAME')
+    block_mask = 1 - tf.nn.max_pool(
+        block_mask, [1, block_size, block_size, 1], [1, 1, 1, 1], "SAME"
+    )
 
     # img = np.array(block_mask[0,:,:,0])
     # plt.imshow(lol)
     # plt.show()
     # input()
 
-    # A = tf.reshape(None, [batch_size, A.shape[1], A.shape[2], A.shape[3]])
     A = tf.cast(A, dtype=tf.float32)
     block_mask = tf.cast(block_mask, dtype=tf.float32)
-
 
     # apply the mask
     applied_mask = tf.multiply(block_mask, A)
 
-    # scale the data
-    # print(tf.cast(tf.size(block_mask), dtype=tf.float32)/tf.reduce_sum(block_mask))
-
-    scaled_data = applied_mask * tf.cast(tf.size(block_mask), dtype=tf.float32)/tf.reduce_sum(block_mask)
-    scaled_data = tf.keras.backend.cast(scaled_data,"float32")
+    scaled_data = (
+        applied_mask
+        * tf.cast(tf.size(block_mask), dtype=tf.float32)
+        / tf.reduce_sum(block_mask)
+    )
+    scaled_data = tf.keras.backend.cast(scaled_data, "float32")
 
     return scaled_data
+
 
 def drop_block_output(A):
     return [batch_size, A[1], A[2], A[3]]
 
-def resnet_layer(inputs,
-                 num_filters=16,
-                 kernel_size=3,
-                 strides=1,
-                 activation='relu',
-                 batch_normalization=True,
-                 conv_first=True):
 
-    conv = Conv2D(num_filters,
-                  kernel_size=kernel_size,
-                  strides=strides,
-                  padding='same',
-                  kernel_initializer='he_normal',
-                  kernel_regularizer=l2(1e-4))
+def resnet_layer(
+    inputs,
+    num_filters=16,
+    kernel_size=3,
+    strides=1,
+    activation="relu",
+    batch_normalization=True,
+    conv_first=True,
+):
+
+    conv = Conv2D(
+        num_filters,
+        kernel_size=kernel_size,
+        strides=strides,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=l2(1e-4),
+    )
 
     x = inputs
     if conv_first:
@@ -154,6 +171,7 @@ def resnet_layer(inputs,
         x = conv(x)
     return x
 
+
 def resnet_v2(input_shape, depth, num_classes=100):
 
     # Start model definition.
@@ -163,14 +181,12 @@ def resnet_v2(input_shape, depth, num_classes=100):
     inputs = Input(shape=input_shape)
 
     # v2 performs Conv2D with BN-ReLU on input before splitting into 2 paths
-    x = resnet_layer(inputs=inputs,
-                     num_filters=num_filters_in,
-                     conv_first=True)
+    x = resnet_layer(inputs=inputs, num_filters=num_filters_in, conv_first=True)
 
     # Instantiate the stack of residual units
     for stage in range(3):
         for res_block in range(num_res_blocks):
-            activation = 'relu'
+            activation = "relu"
             batch_normalization = True
             strides = 1
             if stage == 0:
@@ -181,89 +197,90 @@ def resnet_v2(input_shape, depth, num_classes=100):
             else:
                 num_filters_out = num_filters_in * 2
                 if res_block == 0:  # first layer but not first stage
-                    strides = 2    # downsample
+                    strides = 2  # downsample
 
             # bottleneck residual unit
-            y = resnet_layer(inputs=x,
-                             num_filters=num_filters_in,
-                             kernel_size=1,
-                             strides=strides,
-                             activation=activation,
-                             batch_normalization=batch_normalization,
-                             conv_first=False)
-            y = resnet_layer(inputs=y,
-                             num_filters=num_filters_in,
-                             conv_first=False)
-            y = resnet_layer(inputs=y,
-                             num_filters=num_filters_out,
-                             kernel_size=1,
-                             conv_first=False)
+            y = resnet_layer(
+                inputs=x,
+                num_filters=num_filters_in,
+                kernel_size=1,
+                strides=strides,
+                activation=activation,
+                batch_normalization=batch_normalization,
+                conv_first=False,
+            )
+            y = resnet_layer(inputs=y, num_filters=num_filters_in, conv_first=False)
+            y = resnet_layer(
+                inputs=y, num_filters=num_filters_out, kernel_size=1, conv_first=False
+            )
             if res_block == 0:
                 # linear projection residual shortcut connection to match
                 # changed dims
-                x = resnet_layer(inputs=x,
-                                 num_filters=num_filters_out,
-                                 kernel_size=1,
-                                 strides=strides,
-                                 activation=None,
-                                 batch_normalization=False)
+                x = resnet_layer(
+                    inputs=x,
+                    num_filters=num_filters_out,
+                    kernel_size=1,
+                    strides=strides,
+                    activation=None,
+                    batch_normalization=False,
+                )
             x = keras.layers.add([x, y])
-            if ( stage == 1 or stage == 2 ):
-                x = Dropout(.2)(x)
+            if stage == 1 or stage == 2:
+                x = Dropout(0.2)(x)
 
-            if ( stage == 1 or stage == 2 ):
+            if stage == 1 or stage == 2:
                 layer = Lambda(drop_block, drop_block_output)
                 x = layer(x)
-                # x = drop_block(x)
-                print('hi',x.shape)
                 # x = Dropout(.2)(x)
 
         num_filters_in = num_filters_out
 
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    x = Activation("relu")(x)
     x = AveragePooling2D(pool_size=8)(x)
     y = Flatten()(x)
-    outputs = Dense(num_classes,
-                    activation='softmax',
-                    kernel_initializer='he_normal')(y)
+    outputs = Dense(num_classes, activation="softmax", kernel_initializer="he_normal")(
+        y
+    )
 
     # Instantiate model.
     model = Model(inputs=inputs, outputs=outputs)
     return model
 
+
 if __name__ == "__main__":
     (x_train, y_train), (x_test, y_test) = cifar100.load_data()
     input_shape = x_train.shape[1:]
-    x_train = x_train.astype('float32') / 255
-    x_test = x_test.astype('float32') / 255
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
 
     x_train_mean = np.mean(x_train, axis=0)
     x_train -= x_train_mean
     x_test -= x_train_mean
 
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
-    print('y_train shape:', y_train.shape)
+    print("x_train shape:", x_train.shape)
+    print(x_train.shape[0], "train samples")
+    print(x_test.shape[0], "test samples")
+    print("y_train shape:", y_train.shape)
 
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
 
     model = resnet_v2(input_shape=input_shape, depth=depth)
 
-    model.compile(loss='categorical_crossentropy',
-                optimizer=Adam(learning_rate=lr_schedule(0)),
-                metrics=['accuracy'])
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=Adam(learning_rate=lr_schedule(0)),
+        metrics=["accuracy"],
+    )
     model.summary()
     print(model_type)
 
     lr_scheduler = LearningRateScheduler(lr_schedule)
 
-    lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
-                                cooldown=0,
-                                patience=5,
-                                min_lr=0.5e-6)
+    lr_reducer = ReduceLROnPlateau(
+        factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-6
+    )
 
     callbacks = [lr_reducer, lr_scheduler]
 
@@ -289,15 +306,15 @@ if __name__ == "__main__":
         # randomly shift images vertically
         height_shift_range=0.1,
         # set range for random shear
-        shear_range=0.,
+        shear_range=0.0,
         # set range for random zoom
-        zoom_range=0.,
+        zoom_range=0.0,
         # set range for random channel shifts
-        channel_shift_range=0.,
+        channel_shift_range=0.0,
         # set mode for filling points outside the input boundaries
-        fill_mode='nearest',
+        fill_mode="nearest",
         # value used for fill_mode = "constant"
-        cval=0.,
+        cval=0.0,
         # randomly flip images
         horizontal_flip=True,
         # randomly flip images
@@ -309,20 +326,24 @@ if __name__ == "__main__":
         # image data format, either "channels_first" or "channels_last"
         data_format=None,
         # fraction of images reserved for validation (strictly between 0 and 1)
-        validation_split=0.1)
+        validation_split=0.1,
+    )
 
     # Compute quantities required for featurewise normalization
     # (std, mean, and principal components if ZCA whitening is applied).
     datagen.fit(x_train)
 
     # Fit the model on the batches generated by datagen.flow().
-    model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),
-                        validation_data=(x_test, y_test),
-                        epochs=epochs, verbose=1, workers=4,
-                        callbacks=callbacks)
-
+    model.fit_generator(
+        datagen.flow(x_train, y_train, batch_size=batch_size),
+        validation_data=(x_test, y_test),
+        epochs=epochs,
+        verbose=1,
+        workers=4,
+        callbacks=callbacks,
+    )
 
     # Score trained model.
     scores = model.evaluate(x_test, y_test, verbose=1)
-    print('Test loss:', scores[0])
-    print('Test accuracy:', scores[1])
+    print("Test loss:", scores[0])
+    print("Test accuracy:", scores[1])
